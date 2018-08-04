da756f05650549a9c1a615af5c116ea92438e6bf
Matthew Curran <five3183@gmail.com>
2018-08-04 11:57:16 -0400

    initial push

---
+++ p3/src/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import Login from "./components/Login/Login"
+
+import logo from './logo.svg';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <Login />
+    );
+  }
+}
+
+export default App;
